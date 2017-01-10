91e8da103135d0925a2a6fcbc68999a05756e59f
Muhammad Luqman <MuhammadLuqman@muhammads-macbook-pro.local>
2017-01-10 17:04:27 +0500

    Initial Commit

---
+++ Love Messages/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Love Messages
+//
+//  Created by Muhammad Luqman on 1/10/17.
+//  Copyright © 2017 Muhammad Luqman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
